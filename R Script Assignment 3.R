7db55a4e6119c7c9221d410f01722786fe693d06
JojoKn <h12113366@s.wu.ac.at>
2022-05-08 18:13:24 +0200

    First setup

---
+++ R Script Assignment 3.R
@@ -0,0 +1,28 @@
+###Assignment 3###
+
+###loading the required packages
+
+library(BVAR)
+library(vars)
+library(fredr)
+
+###########################################
+###Estimation and Identification of VARs###
+
+###1###
+
+###2###
+
+###3###
+
+###4###
+
+###5###
+
+###6###
+
+
+##########################
+###Bonus: Bayesian VARs###
+
+###1###
